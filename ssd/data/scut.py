73e8268a267cd43835996eb12c8d554581714a9b
olgaiv39 <ivolga.vak@gmail.com>
2019-01-24 02:57:59 +0300

    Add SSD model

---
+++ ssd/data/scut.py
@@ -0,0 +1,114 @@
+import os.path as os
+import sys
+import torch
+import torch.utils.data as data
+import cv2
+import numpy as np
+if sys.version_info[0] == 2:
+    import xml.etree.cElementTree as ET
+else:
+    import xml.etree.ElementTree as ET
+
+SCUT_CLASSES = (  # always index 0
+ 'person')
+
+SCUT_ROOT = os.join("../ssd/dataset/")
+
+
+class SCUTAnnotationTransform(object):
+
+    def __init__(self, class_to_ind=None, keep_difficult=False):
+        self.class_to_ind = class_to_ind or dict(
+            zip(SCUT_CLASSES, range(len(SCUT_CLASSES))))
+        self.keep_difficult = keep_difficult
+
+    def __call__(self, target, width, height):
+        res = []
+        for obj in target.iter('object'):
+            difficult = int(obj.find('difficult').text) == 1
+            if not self.keep_difficult and difficult:
+                continue
+            name = obj.find('name').text.lower().strip()
+            bbox = obj.find('bndbox')
+
+            pts = ['xmin', 'ymin', 'xmax', 'ymax']
+            bndbox = []
+            for i, pt in enumerate(pts):
+                cur_pt = int(bbox.find(pt).text) - 1
+                # scale height or width
+                cur_pt = cur_pt / width if i % 2 == 0 else cur_pt / height
+                bndbox.append(cur_pt)
+            label_idx = self.class_to_ind[name]
+            bndbox.append(label_idx)
+            res += [bndbox]  # [xmin, ymin, xmax, ymax, label_ind]
+            img_id = target.find('filename').text[:-4]
+
+        return res  [[xmin, ymin, xmax, ymax, label_ind], ... ]
+
+
+class SCUTDetection(data.Dataset):
+    def __init__(self, root,
+                 image_sets=['SCUT_HEAD_Part_A', 'SCUT_HEAD_Part_B '],
+                 transform=None, target_transform=SCUTAnnotationTransform(),
+                 dataset_name='SCUT_HEAD'):
+        self.root = root
+        self.image_set = image_sets
+        self.transform = transform
+        self.target_transform = target_transform
+        self.name = dataset_name
+        self._annopath = os.join('%s', 'Annotations', '%s.xml')
+        self._imgpath = os.join('%s', 'JPEGImages', '%s.jpg')
+        self.ids = list()
+        for (name) in image_sets:
+            rootpath = os.join(self.root, 'SCUT_HEAD_Part_A')
+            for line in open(os.join(rootpath, 'ImageSets', 'Main', name, '.txt')):
+                self.ids.append((rootpath, line.strip()))
+
+    def __getitem__(self, index):
+        im, gt, h, w = self.pull_item(index)
+
+        return im, gt
+
+    def __len__(self):
+        return len(self.ids)
+
+    def pull_item(self, index):
+        img_id = self.ids[index]
+
+        target = ET.parse(self._annopath % img_id).getroot()
+        img = cv2.imread(self._imgpath % img_id)
+        height, width, channels = img.shape
+
+        if self.target_transform is not None:
+            target = self.target_transform(target, width, height)
+
+        if self.transform is not None:
+            target = np.array(target)
+            img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
+            # to rgb
+            img = img[:, :, (2, 1, 0)]
+            # img = img.transpose(2, 0, 1)
+            target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
+        return torch.from_numpy(img).permute(2, 0, 1), target, height, width
+        # return torch.from_numpy(img), target, height, width
+
+    def pull_image(self, index):
+        img_id = self.ids[index]
+        return cv2.imread(self._imgpath % img_id, cv2.IMREAD_COLOR)
+
+    def pull_anno(self, index):
+        img_id = self.ids[index]
+        anno = ET.parse(self._annopath % img_id).getroot()
+        gt = self.target_transform(anno, 1, 1)
+        return img_id[1], gt
+
+    def pull_tensor(self, index):
+        return torch.Tensor(self.pull_image(index)).unsqueeze_(0)
+
+def get_label_map(label_file):
+    label_map = {}
+    labels = open(label_file, 'r')
+    for line in labels:
+        ids = line.split(',')
+        label_map[int(ids[0])] = int(ids[1])
+    return label_map
